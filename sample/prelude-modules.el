aa69c57f2e3a476bcbad62096934455b515f7219
Markus Maucher <markus.maucher@uni-ulm.de>
2014-03-21 17:54:49 +0100

    added some requires

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -4,10 +4,10 @@
 ;; (require 'prelude-helm) ;; Interface for narrowing and search
 
 (require 'prelude-c)
-;; (require 'prelude-clojure)
+(require 'prelude-clojure)
 ;; (require 'prelude-coffee)
-;; (require 'prelude-common-lisp)
-;; (require 'prelude-css)
+(require 'prelude-common-lisp)
+(require 'prelude-css)
 (require 'prelude-emacs-lisp)
 (require 'prelude-erc) ;; Emacs IRC client
 ;; (require 'prelude-erlang)
@@ -20,9 +20,9 @@
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
 ;; (require 'prelude-python)
-;; (require 'prelude-ruby)
+(require 'prelude-ruby)
 ;; (require 'prelude-scala)
-(require 'prelude-scheme)
+;; (require 'prelude-scheme)
 ;; (require 'prelude-scss)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
