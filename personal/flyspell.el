2af87cf8dc5e70370296c77a22739b4f1485723a
Markus Maucher <markus.maucher@uni-ulm.de>
2014-04-07 16:07:53 +0200

    Added sbcl.el and flyspell.el

--- personal/flyspell.el
+++ personal/flyspell.el
@@ -1,1 +1,9 @@
+;;; flyspell .. Summary
+
+;;; Commentary:
+
+;;; Code:
 (ispell-change-dictionary "english")
+(setq flyspell-default-dictionary "english")
+
+(provide 'flyspell)
--- personal/sbcl.el
+++ personal/sbcl.el
@@ -1,5 +1,14 @@
+;;; Package --- Summary
+;;; Set paths for some LISP implementations
+
+;;; Commentary:
+
+;;; Code:
 (setq slime-default-lisp 'sbcl)
 (setq slime-lisp-implementations
      `((sbcl ("/usr/bin/sbcl"))
        (ecl ("/usr/bin/ecl"))
        (clisp ("/usr/bin/clisp" "-q -I"))))
+
+(provide 'sbcl)
+;;; sbcl.el ends here
