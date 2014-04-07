2deeaa85082386a9ffc6721b0aae153e58bcc9df
Markus Maucher <markus.maucher@uni-ulm.de>
2014-04-07 17:09:59 +0200

    Mac keys

---
+++ personal/mac-keys.el
@@ -0,0 +1,13 @@
+;;; Package --- Summary
+;;; Set Apple key as Meta
+
+;;; Commentary:
+
+;;; Code:
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
+(provide 'mac-keys)
+;;; mac-keys.el ends here
